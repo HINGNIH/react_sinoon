3d278cabffdac359de4ef37f2aa689e7e1ee7627
HINGNIH <dkstks1409@gmail.com>
2024-11-18 17:55:50 +0900

    Add build files

---
+++ src/pages/cart/index.jsx
@@ -0,0 +1,21 @@
+import CartList from '../../components/cart/CartList';
+import { CartWrap } from './style';
+import CartEmpty from '../../components/cart/CartEmpty';
+import { useSelector } from 'react-redux';
+
+const Cart = () => {
+    const {carts} = useSelector((state)=>state.cart)
+    if(carts.length==0) return <CartEmpty />
+    if(carts.length>0) return  <CartList />
+    
+    return (
+        <CartWrap className='cart-box'>
+            <div className='inner'>
+                {/* <CartList />
+                <CartEmpty /> */}
+            </div>
+        </CartWrap>
+    );
+};
+
+export default Cart;
